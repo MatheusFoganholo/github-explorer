f052ab202cb5543d902e51f05a7834c4aed5f71c
Matheus Foganholo <matheusfoganholo@hotmail.com>
2020-11-18 17:41:51 -0300

    Dashboard styled

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/56047758?v=4"
+            alt="Matheus Foganholo"
+          />
+          <div>
+            <strong>MatheusFoganholo/ng-agenda</strong>
+            <p>
+              A single page application from a simple schedule that stores the
+              contacts name, address and phone number locally. Created with
+              Angular.
+            </p>
+          </div>
+
+          <FiChevronRight size={30} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,92 @@
+import { FiChevronRight } from 'react-icons/fi';
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
+  margin-top: 80px;
   font-size: 48px;
+  max-width: 450px;
+  line-height: 56px;
   color: #3a3a3a;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    background-color: #04d361;
+    border: 0;
+    border-radius: 0 5px 5px 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background-color: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+
+  a {
+    background-color: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
